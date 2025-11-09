acb3f976a225b7f00f3f6204fe25960c8f01cfb5
Laura Baptista <l.lochetti@gmail.com>
2025-11-09 11:05:02 -0300

    PAXE-4 configurado kafka e criado tópicos para cada domínio

---
+++ docker/kafka/create_topics.py
@@ -0,0 +1,94 @@
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka import KafkaException
+import time
+
+
+# --------------------------------------------------------
+# CONFIGURAÇÕES
+# --------------------------------------------------------
+KAFKA_BOOTSTRAP = "kafka:9092"
+
+# Tópicos por domínio
+topics = [
+    # Domínio Clientes
+    "clients.create.v1",
+    "clients.update.v1",
+    "clients.delete.v1",
+    "clients.auth.v1",
+    "clients.address.update.v1",
+
+    # Domínio Vendas
+    "sales.order.create.v1",
+    "sales.order.update.v1",
+    "sales.order.cancel.v1",
+    "sales.order.status.v1",
+    "sales.order.completed.v1",
+]
+
+PARTITIONS = 3
+REPLICATION = 1
+
+
+# --------------------------------------------------------
+# AGUARDAR O KAFKA FICAR ONLINE
+# --------------------------------------------------------
+def wait_for_kafka(bootstrap):
+    admin = AdminClient({"bootstrap.servers": bootstrap})
+
+    for t in range(5):
+        try:
+            admin.list_topics(timeout=3)
+            return True
+        except KafkaException:
+            print("WARN: Kafka ainda não disponível. Tentando novamente")
+            time.sleep(2 ** t)
+
+    return False
+
+
+# --------------------------------------------------------
+# CRIAÇÃO DOS TÓPICOS
+# --------------------------------------------------------
+def create_kafka_topics():
+    admin = AdminClient({
+        "bootstrap.servers": KAFKA_BOOTSTRAP,
+        "client.id": "topic_creator"
+    })
+
+    new_topics = [
+        NewTopic(
+            topic,
+            num_partitions=PARTITIONS,
+            replication_factor=REPLICATION,
+        )
+        for topic in topics
+    ]
+
+    print("INFO: Criando tópicos")
+
+    # create_topics retorna Futures
+    results = admin.create_topics(new_topics, request_timeout=15)
+
+    for topic, future in results.items():
+        try:
+            future.result()
+            print(f"INFO: Criado: {topic}")
+        except KafkaException as e:
+            if "Topic already exists" in str(e):
+                print(f"WARN: Já existe: {topic}")
+            else:
+                print(f"ERROR: Erro ao criar {topic}: {e}")
+
+
+# --------------------------------------------------------
+# EXECUÇÃO
+# --------------------------------------------------------
+if __name__ == "__main__":
+    print("INFO: Aguardando Kafka ficar online...")
+
+    if not wait_for_kafka(KAFKA_BOOTSTRAP):
+        print("ERROR: Kafka não subiu a tempo. Abortando.")
+        exit(1)
+
+    print("\nINFO: Kafka online! Criando tópicos...\n")
+    create_kafka_topics()
